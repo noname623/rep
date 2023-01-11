5b2889eb99670093503fe74b6bb9cb616d67b474
Gudin Ivan <vangud0829@gmail.com>
2023-01-11 19:42:59 +0300

    Signed-off-by: Gudin Ivan <vangud0829@gmail.com>

--- IdeaProjects/Gudin/src/Main.java
+++ IdeaProjects/Gudin/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("HELLOW EMAE");
+        System.out.println("HELLOW EME");
     }
 }
